142f4f7fb5ed97595eba051788b2e5ecc9a7652a
acanimal <asantiago@acuriousanimal.com>
2015-05-27 16:07:24 +0200

    Reviewer code linted

--- bin/index.js
+++ bin/index.js
@@ -1,67 +1,8 @@
 #!/usr/bin/env node
 
+/*eslint no-use-before-define:0, no-process-exit:0*/
+
 "use strict";
-
-/**
- * Normalize a port into a number, string, or false.
- * @param {Number} val Port number to use
- * @returns {Number} Normalized value or false otherwise
- */
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind = typeof port === "string"
-    ? "Pipe " + port
-    : "Port " + port
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string"
-    ? "pipe " + addr
-    : "port " + addr.port;
-  console.log("Clyde is listening on " + bind);
-}
-
 
 /**
  * Module dependencies.
@@ -90,3 +31,69 @@ var server = http.createServer(clydeProxy);
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ * @param {Number} val Port number to use
+ * @returns {Number} Normalized value or false otherwise
+ */
+function normalizePort(val) {
+  var portNumber = parseInt(val, 10);
+
+  if (isNaN(portNumber)) {
+    // named pipe
+    return val;
+  }
+
+  if (portNumber >= 0) {
+    // port number
+    return portNumber;
+  }
+
+  return false;
+}
+
+
+/**
+ * Event listener for HTTP server "error" event.
+ * @param {number} error Error number
+ * @returns {void}
+ * @throws {Error}
+ */
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string"
+    ? "Pipe " + port
+    : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+/**
+ * Event listener for HTTP server "listening" event.
+ * @returns {void}
+ */
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string"
+    ? "pipe " + addr
+    : "port " + addr.port;
+  console.log("Clyde is listening on " + bind);
+}
--- plugins/logger/index.js
+++ plugins/logger/index.js
@@ -1,3 +1,4 @@
+"use strict";
 
 console.log("This is the logger middleware !!!");
 
