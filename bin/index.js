1c6cc7c8fcf682830679c2ce0294531b755b706f
acanimal <asantiago@acuriousanimal.com>
2015-06-04 18:57:59 +0200

    Load options via module

--- bin/index.js
+++ bin/index.js
@@ -5,37 +5,10 @@
 "use strict";
 
 var http = require("http"),
-    path = require("path");
-
-/**
- * Parse command line arguments
- */
-var argv = require("yargs")
-  .usage("Usage: $0 [options]")
-  .example("$0 --log info", "Start clyde with log messages on 'info' level")
-  .describe("logfile", "Path to the log file. Default 'clyde.log'.")
-  .nargs('logfile', 1)
-  .describe("loglevel", "Level used for clyde log messages. Default 'info'.")
-  .nargs('loglevel', 1)
-  .describe("port", "Port where clyde will listen. Default 8080.")
-  .nargs('port', 1)
-  .help("help")
-  .showHelpOnFail(false, "Specify --help for available options")
-  .argv;
-
-/**
- * Set global options and initialize logger
- */
-var options = {
-  logfile: argv.logfile || "clyde.log",
-  loglevel: argv.loglevel || "info",
-  port: argv.port || "8080"
-};
-
-console.log(options);
-
-var log = require("../lib/log").log(options);
-var config = require("../config"),
+    path = require("path"),
+    options = require("../lib/cli-options"),
+    log = require("../lib/log"),
+    config = require("../config"),
     clyde = require("../lib/clyde");
 
 /**
