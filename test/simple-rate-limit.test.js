5562973199bf2bcd80ef7e96b8715726f587973e
acanimal <asantiago@acuriousanimal.com>
2015-06-12 19:58:19 +0200

    Rate limit tests

--- test/simple-rate-limit.test.js
+++ test/simple-rate-limit.test.js
@@ -1,4 +1,4 @@
-/* eslint no-unused-vars:0 */
+/* eslint no-unused-vars:0, no-unused-expressions:0 */
 "use strict";
 
 var path = require("path");
@@ -13,6 +13,7 @@ describe("simple-rate-limit", function() {
     configuration.base(path.join(__dirname, ".."));
   });
 
+
   it("should fail due global limit exceed", function(done) {
     var configJS = require("./fixtures/config-rate-limit-global.json");
     var config = configuration.load(configJS);
@@ -20,15 +21,66 @@ describe("simple-rate-limit", function() {
 
     // Configuration allows 1 call per second, so first call must be fine and
     // the second must fail.
-    rateLimit.middleware({}, {}, function(err){
+    rateLimit.middleware({}, {}, function(err) {
       expect(err).to.be.undefined;
     });
 
-    rateLimit.middleware({}, {}, function(err){
+    rateLimit.middleware({}, {}, function(err) {
       expect(err.name).to.be.equal("RateLimitExceeded");
     });
 
     done();
+  });
+
+
+  it("should fail due consumer limit exceed", function() {
+    var configJS = require("./fixtures/config-rate-limit-consumer.json");
+    var config = configuration.load(configJS);
+    var rateLimit = config.getPrefilterByName("rate-limit");
+
+    // Configuration allows 1 call per second, so first call must be fine and
+    // the second must fail.
+    rateLimit.middleware({user: {userId: "userA"}}, {}, function(err) {
+      expect(err).to.be.undefined;
+    });
+
+    rateLimit.middleware({user: {userId: "userA"}}, {}, function(err) {
+      expect(err.name).to.be.equal("RateLimitExceeded");
+    });
+  });
+
+
+  it("should fail due provider global limit exceed", function() {
+    var configJS = require("./fixtures/config-rate-limit-provider-global.json");
+    var config = configuration.load(configJS);
+    var rateLimit = config.getPrefilterByName("rate-limit");
+
+    // Configuration allows 1 call per second, so first call must be fine and
+    // the second must fail.
+    rateLimit.middleware({provider: "/providerA"}, {}, function(err) {
+      expect(err).to.be.undefined;
+    });
+
+    rateLimit.middleware({provider: "/providerA"}, {}, function(err) {
+      expect(err.name).to.be.equal("RateLimitExceeded");
+    });
+  });
+
+
+  it("should fail due provider-consumer limit exceed", function() {
+    var configJS = require("./fixtures/config-rate-limit-provider-consumer.json");
+    var config = configuration.load(configJS);
+    var rateLimit = config.getPrefilterByName("rate-limit");
+
+    // Configuration allows 1 call per second, so first call must be fine and
+    // the second must fail.
+    rateLimit.middleware({provider: "/providerA", user: {userId: "userA"}}, {}, function(err) {
+      expect(err).to.be.undefined;
+    });
+
+    rateLimit.middleware({provider: "/providerA", user: {userId: "userA"}}, {}, function(err) {
+      expect(err.name).to.be.equal("RateLimitExceeded");
+    });
   });
 
 });
