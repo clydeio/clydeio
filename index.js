16ae512cd30d21ca89f7d8f80e7a03588ade2e5c
acanimal <asantiago@acuriousanimal.com>
2015-07-01 22:33:24 +0200

    Allow creation of more than one clyde middleware

--- index.js
+++ index.js
@@ -1,102 +1,38 @@
 #!/usr/bin/env node
 
-// try{
-//   var notexists = require("notexists");
-// } catch(err) {
-//   console.log("error ", err.code);
-// }
-
-// console.log("not ", notexists);
-
-// process.exit(0);
-// console.log("not shown");
-
-
-/*eslint no-use-before-define:0, no-process-exit:0*/
-
 "use strict";
 
 var http = require("http"),
     options = require("./lib/options"),
     clyde = require("./lib/clyde");
 
-/**
- * Create proxy HTTP server.
- */
-var clydeProxy = clyde.createProxyServer(options);
-var server = http.createServer(clydeProxy);
+// Create Clyde middleware
+var middleware = clyde.createMiddleware(options);
 
+// Create server and listen on port
+var server = http.createServer(middleware);
+server.listen(options.port);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-var port = normalizePort(options.port);
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-
-/**
- * Normalize a port into a number, string, or false.
- * @param {Number} val Port number to use
- * @returns {Number} Normalized value or false otherwise
- */
-function normalizePort(val) {
-  var portNumber = parseInt(val, 10);
-
-  if (isNaN(portNumber)) {
-    // named pipe
-    return val;
-  }
-
-  if (portNumber >= 0) {
-    // port number
-    return portNumber;
-  }
-
-  return false;
-}
-
-
-/**
- * Event listener for HTTP server "error" event.
- * @param {number} error Error number
- * @returns {void}
- * @throws {Error}
- */
-function onError(error) {
+server.on("error", function(error) {
   if (error.syscall !== "listen") {
     throw error;
   }
-
-  var bind = typeof port === "string"
-    ? "Pipe " + port
-    : "Port " + port;
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
+      console.error("Port " + options.port + " requires elevated privileges");
+      process.exit(1);  // eslint-disable-line no-process-exit
       break;
     case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
+      console.error("Port " + options.port + " is already in use");
+      process.exit(1);  // eslint-disable-line no-process-exit
       break;
     default:
       throw error;
   }
-}
+});
 
-
-/**
- * Event listener for HTTP server "listening" event.
- * @returns {void}
- */
-function onListening() {
+server.on("listening", function() {
   var addr = server.address();
-  var bind = typeof addr === "string"
-    ? "pipe " + addr
-    : "port " + addr.port;
-  console.info("Clyde is listening on " + bind);
-}
+  console.info("Clyde is listening on port " + addr.port);
+});
