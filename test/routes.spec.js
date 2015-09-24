709f48e870baef109487eab8d17b2ed39e220a2c
acanimal <asantiago@acuriousanimal.com>
2015-09-24 14:50:01 +0200

    Tested operations for conusmers

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+
 "use strict";
 
 var repository = require("../lib/butler/memory-repository"),
@@ -23,6 +25,31 @@ describe("routes (memory backend)", function() {
   });
 
 
+
+  //
+  // Filters
+  //
+  // describe("filters", function() {
+  //
+  //   var filterId = null;
+  //
+  //   it("'[GET] /filters' should get an emtpy filters array", function(done) {
+  //     request("http://localhost:9999")
+  //       .get("/filters")
+  //       .set("Accept", "application/json")
+  //       .expect("Content-Type", "application/json; charset=utf-8")
+  //       .expect(200)
+  //       .expect(function(res) {
+  //         expect(res.body).to.be.instanceOf(Array);
+  //         expect(res.body).to.be.emtpy;
+  //       })
+  //       .end(done);
+  //   });
+  //
+  // });
+
+
+
   //
   // Consumers
   //
@@ -38,7 +65,7 @@ describe("routes (memory backend)", function() {
         .expect(200)
         .expect(function(res) {
           expect(res.body).to.be.instanceOf(Array);
-          expect(res.body).to.be.emtpy; // eslint-disable-line no-unused-expressions
+          expect(res.body).to.be.emtpy;
         })
         .end(done);
     });
@@ -109,6 +136,20 @@ describe("routes (memory backend)", function() {
 
           consumerId = res.body.id;
         })
+        .end(done);
+    });
+
+    it("'[POST] /consumers' fails due duplicated key", function(done) {
+      var props = {
+        key: "01234567890123456789",
+        secret: "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+      };
+      request("http://localhost:9999")
+        .post("/consumers")
+        .send(props)
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(409)
         .end(done);
     });
 
@@ -184,297 +225,38 @@ describe("routes (memory backend)", function() {
         .end(done);
     });
 
+    it("'[PUT] /consumers' fails due duplicated key", function(done) {
+      var props = {
+        key: "98765432109876543210",
+        secret: "1234567890abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+      };
+      request("http://localhost:9999")
+        .put("/consumers/" + consumerId)
+        .send(props)
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(409)
+        .end(done);
+    });
+
+    it("'[DELETE] /consumers' fails due not existent consumer", function(done) {
+      request("http://localhost:9999")
+        .delete("/consumers/notexists")
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(404)
+        .end(done);
+    });
+
+    it("'[DELETE] /consumers' should success deleteing a consumer", function(done) {
+      request("http://localhost:9999")
+        .delete("/consumers/" + consumerId)
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(200)
+        .end(done);
+    });
+
   });
-
-
-
-
-  // describe("providers", function() {
-
-    // var providerId; // eslint-disable-line no-unused-vars
-
-    // it("should get an emtpy providers array", function(done) {
-    //   request("http://localhost:9999")
-    //     .get("/providers")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(200)
-    //     .expect(function(res) {
-    //       expect(res.body).to.be.instanceOf(Array);
-    //       expect(res.body).to.be.emtpy; // eslint-disable-line no-unused-expressions
-    //     })
-    //     .end(done);
-    // });
-    //
-    // it("fail getting a providers with non existent 'id'", function(done) {
-    //   request("http://localhost:9999")
-    //     .get("/providers/notexists")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(404)
-    //     .expect(function(res) {
-    //       expect(res.body).to.be.instanceOf(Object);
-    //       expect(res.body).to.be.emtpy; // eslint-disable-line no-unused-expressions
-    //     })
-    //     .end(done);
-    // });
-
-    // it("fail adding a new provider due with no data", function(done) {
-    //   request("http://localhost:9999")
-    //     .post("/providers")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(400, done);
-    // });
-    //
-    // it("fail adding a new provider due no 'context' specified", function(done) {
-    //   var provider = {
-    //     id: "pid",
-    //     target: "http://targetserver"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .post("/providers")
-    //     .send(provider)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(400, done);
-    // });
-    //
-    // it("fail adding a new provider due no 'target' specified", function(done) {
-    //   var provider = {
-    //     id: "pid",
-    //     context: "/pid"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .post("/providers")
-    //     .send(provider)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(400, done);
-    // });
-    //
-    // it("should add a new provider", function(done) {
-    //   var provider = {
-    //     id: "pid",
-    //     context: "/pid",
-    //     target: "http://targetserver"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .post("/providers")
-    //     .send(provider)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json")
-    //     .end(function(err, res) {   // eslint-disable-line handle-callback-err
-    //       expect(res.status).to.be.equal(200);
-    //       // Store id for next tests.
-    //       providerId = res.body.id;
-    //       request("http://localhost:9999")
-    //         .get("/providers")
-    //         .set("Accept", "application/json")
-    //         .expect("Content-Type", "application/json; charset=utf-8")
-    //         .expect(200)
-    //         .expect(function(res2) {
-    //           expect(res2.body).to.be.instanceOf(Array);
-    //           expect(res2.body).to.have.length(1);
-    //         })
-    //         .end(done);
-    //     });
-    // });
-    //
-    // it("should get a providers with specified 'id'", function(done) {
-    //   request("http://localhost:9999")
-    //     .get("/providers/"+providerId)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(200)
-    //     .expect(function(res) {
-    //       expect(res.body).to.be.instanceOf(Object);
-    //       expect(res.body).to.be.not.emtpy; // eslint-disable-line no-unused-expressions
-    //     })
-    //     .end(done);
-    // });
-    //
-    // it("should success updating a provider with no data", function(done) {
-    //   request("http://localhost:9999")
-    //     .put("/providers/"+providerId)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(200)
-    //     .expect(function(res) {
-    //       expect(res.body).to.be.instanceOf(Object);
-    //       expect(res.body).to.be.not.emtpy; // eslint-disable-line no-unused-expressions
-    //       expect(res.body.id).to.be.equal(providerId);
-    //     })
-    //     .end(done);
-    // });
-    //
-    // it("fail updating a provider due id does not exists", function(done) {
-    //   var provider = {
-    //     id: "pid",
-    //     context: "/pid",
-    //     target: "http://targetserver"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .put("/providers/abc")
-    //     .set("Accept", "application/json")
-    //     .send(provider)
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(404, done);
-    // });
-    //
-    // it("should update a provider ignoring the new id propety", function(done) {
-    //   var provider = {
-    //     id: "pid",
-    //     context: "/pid",
-    //     target: "http://targetserver"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .put("/providers/"+providerId)
-    //     .set("Accept", "application/json")
-    //     .send(provider)
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(200)
-    //     .expect(function(res) {
-    //       expect(res.body.id).to.be.equals(providerId);
-    //       expect(res.body.context).to.be.equal(provider.context);
-    //       expect(res.body.target).to.be.equal(provider.target);
-    //     })
-    //     .end(done);
-    // });
-    //
-    // it("should update a provider", function(done) {
-    //   var provider = {
-    //     id: "abc",
-    //     context: "/abc",
-    //     target: "http://anothertargetserver"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .put("/providers/"+providerId)
-    //     .set("Accept", "application/json")
-    //     .send(provider)
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .end(function(err, res) {   // eslint-disable-line handle-callback-err
-    //       expect(res.status).to.be.equal(200);
-    //       request("http://localhost:9999")
-    //         .get("/providers/"+providerId)
-    //         .set("Accept", "application/json")
-    //         .expect("Content-Type", "application/json; charset=utf-8")
-    //         .expect(function(res2) {
-    //           expect(res2.status).to.be.equal(200);
-    //           expect(res2.body.id).to.be.equal(providerId);
-    //           expect(res2.body.context).to.be.equal(provider.context);
-    //           expect(res2.body.target).to.be.equal(provider.target);
-    //         })
-    //         .end(done);
-    //     });
-    // });
-    //
-    // it("fail deleting a non existent provider", function(done) {
-    //   request("http://localhost:9999")
-    //     .delete("/providers/pid")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(404, done);
-    // });
-    //
-    // it("should delete an existent provider", function(done) {
-    //   request("http://localhost:9999")
-    //     .delete("/providers/"+providerId)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .end(function(err, res) {   // eslint-disable-line handle-callback-err
-    //       expect(res.status).to.be.equal(200);
-    //       request("http://localhost:9999")
-    //         .get("/providers/abc")
-    //         .set("Accept", "application/json")
-    //         .expect("Content-Type", "application/json; charset=utf-8")
-    //         .expect(404, done);
-    //     });
-    // });
-
-  // });
-
-  // describe("resources", function() {
-
-    // var providerId;
-
-    // before(function(done) {
-    //   // Create a provider for testing
-    //   var provider = {
-    //     context: "/provider_test",
-    //     target: "http://targetserver"
-    //   };
-    //
-    //   request("http://localhost:9999")
-    //     .post("/providers")
-    //     .send(provider)
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(200)
-    //     .expect(function(res) {
-    //       providerId = res.body.id;
-    //     })
-    //     .end(done);
-    // });
-    //
-    // it("fail getting resources from a non existent provider", function(done) {
-    //   request("http://localhost:9999")
-    //     .get("/providers/abc/resources")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(404, done);
-    // });
-    //
-    // it("should get an emtpy resources array from a provider", function(done) {
-    //   request("http://localhost:9999")
-    //     .get("/providers/" + providerId + "/resources")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(200)
-    //     .expect(function(res) {
-    //       expect(res.body).to.be.instanceOf(Array);
-    //       expect(res.body).to.be.empty; // eslint-disable-line no-unused-expressions
-    //     })
-    //     .end(done);
-    // });
-    //
-    // it("fail getting non existent resource from a provider", function(done) {
-    //   request("http://localhost:9999")
-    //     .get("/providers/" + providerId + "/resources/notexists")
-    //     .set("Accept", "application/json")
-    //     .expect("Content-Type", "application/json; charset=utf-8")
-    //     .expect(404, done);
-    // });
-
-  //   it("fail adding new resources on a provider due no data", function(done) {
-  //     request("http://localhost:9999")
-  //       .post("/providers/pid/resources")
-  //       .set("Accept", "application/json")
-  //       .expect("Content-Type", "application/json; charset=utf-8")
-  //       .expect(400, done);
-  //   });
-  //
-  //   it("fail adding new resources on a provider due no 'context'", function(done) {
-  //     var resource = {
-  //       context: "/res"
-  //     }
-  //     request("http://localhost:9999")
-  //       .post("/providers/pid/resources")
-  //       .set("Accept", "application/json")
-  //       .send(resource)
-  //       .expect("Content-Type", "application/json; charset=utf-8")
-  //       .expect(400, done);
-  //   });
-
-  // });
-
-  // describe("filters", function() {
-  // });
 
 });
